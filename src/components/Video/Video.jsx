01de82adfbb80d3b3f976f3b09ce8316f1896d9d
sebasgp <sebastian.gallegop@udea.edu.co>
2021-10-28 22:04:44 -0500

    Styles video control

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState, useRef } from "react";
 import video from "../../assets/video/video.webm";
+import playIcon from "../../assets/images/play.png";
 import "./video.css";
 
 function Video() {
-  const [playing, setPlaying] = useState(true);
+  const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
 
   const handlePlay = () => {
@@ -19,12 +20,14 @@ function Video() {
 
   return (
     <div className="video-container">
-      <video ref={videoRef} autoPlay loop muted={false}>
+      <video width="320" onClick={handlePlay} ref={videoRef} loop>
         <source src={video} />
       </video>
-      <div className="btn">
-        <button onClick={handlePlay}>Play/Stop</button>
-      </div>
+      {playing ? null : (
+        <div onClick={handlePlay} className="control-container">
+          <img className="play" src={playIcon} alt="" />
+        </div>
+      )}
     </div>
   );
 }
